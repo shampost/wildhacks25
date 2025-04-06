b89d18a882530a61b8cc36f361289ecfe4a6c18b
Fabian Sevilla <fabsevilla@yahoo.com>
2025-04-06 00:17:50 -0500

    included image uploader and some scripts

--- frontend/script.js
+++ frontend/script.js
@@ -2,16 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchBtn = document.getElementById('search');
     const cancelBtn = document.getElementById('cancel');
     const addressInput = document.getElementById('address');
+    const imageUpload = document.getElementById('imageUpload');
+    const file = imageInput.files[0];
+    const reader = new FileReader();
+
   
     // Handler for the search button: sends address data to the backend
     searchBtn.addEventListener('click', () => {
       const address = addressInput.value.trim();
-  
-      if (!address) {
-        alert('Please enter an address.');
+      const image = imageUpload.value;
+
+      if (!address || !imageUpload) {
+        alert('Please enter one of the fields.');
         return;
       }
-  
+    
+      if(adress){
       // Send the address data to a Python backend endpoint (adjust the URL as needed)
       fetch('/search', {
         method: 'POST',
@@ -28,8 +34,41 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => {
           console.error('Error sending address to Python script:', error);
         });
-    });
-  
+}});
+
+      if(file){
+        reader.onloadend = async () => {
+          const IMG = reader.result.split(',')[1];
+        
+
+        try {
+          const response = await fetch('http://localhost:8000/identify-plant',{
+            method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            image_data: base64Image
+          }),
+        });
+
+        const results = await response.json();
+        
+        if (results.success) {
+          resultBox.textContent = `Plant: ${results.plant.name}\n Info: ${results.plant.description || 'N/A'}`;
+        } else {
+          resultBox.textContent = `Error: ${results.message}`;
+        }
+      } catch (error) {
+        resultBox.textContent = `Error: ${error.message}`;
+      }
+    };
+
+    reader.readAsDataURL(file);
+
+      }
+      
+
     // Handler for the cancel button: clears the input field
     cancelBtn.addEventListener('click', () => {
       addressInput.value = '';
